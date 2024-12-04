fe9d4976542787e9ca8320e87a6a75169acd9d79
Bruger <samo0004@stud.kea.dk>
2024-12-04 11:40:49 +0100

    inti commit

---
+++ backend/api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'S98156730ara!',
+    database: 'mydatabase'
+});
